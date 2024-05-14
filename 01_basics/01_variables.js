76aa270173fd5e0c696ffb53fb0b7a2a55d693ed
Ravi Prakash Chaurasiya <113404805+rp7007@users.noreply.github.com>
2024-05-14 14:54:37 +0000

    vaiables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 134567
+let accountEmail = "rp@google.com"
+var accountPassword = "12345"
+accountCity = "SantKabirNagar"
+
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "rr@pc.com"
+accountPassword = "3243567"
+accountCity = "Gorakhpur"
+
+console.log(accountId)
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
