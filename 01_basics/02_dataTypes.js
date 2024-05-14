a4af71dc5a1943df7a4624ec0a64358ea0f623d5
Ravi Prakash Chaurasiya <113404805+rp7007@users.noreply.github.com>
2024-05-14 17:21:37 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strics"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+    3) // code readability should be high
+
+console.log("RP");
+
+let name = "Ravi"
+let age = 20
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => 
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
