52d27fee918535e8f17db06171ba8b7cb1fda45f
Ravi Prakash Chaurasiya <113404805+rp7007@users.noreply.github.com>
2024-05-15 02:50:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Ravi";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Number Conversion
+// "33" => 33
+// "33abc" => NaN
+// true => 1; flase => 0
+
+
+let isLoggedIn = "Ravi";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
